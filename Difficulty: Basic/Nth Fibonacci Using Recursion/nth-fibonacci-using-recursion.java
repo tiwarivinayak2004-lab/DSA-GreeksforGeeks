e9b3931c21827dbf949eba9003566594c55600e4
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-08-22 13:07:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Nth Fibonacci Using Recursion/nth-fibonacci-using-recursion.java
@@ -0,0 +1,8 @@
+class Solution {
+    static int nthFibonacci(int n) {
+        // code here
+        if(n==0 || n==1) return n;
+        
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+}

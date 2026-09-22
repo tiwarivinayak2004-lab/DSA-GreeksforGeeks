9638e61de560f2665ef9a5cffffb0f0276563646
Vinayak Tiwari <tiwarivinayak2004@gmail.com>
2026-09-22 14:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a Linked List/reverse-a-linked-list.java
@@ -0,0 +1,27 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    Node reverseList(Node head) {
+        // code here
+        Node curr=head;
+        Node prev=null;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
